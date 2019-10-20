ccaa6e82bfd0e88faeb98a37518341cec6ddfc65
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-20 15:56:38 +0300

    Init server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Server')
+const dotenv = require('dotenv')
+const express = require('express')
+
+// Load .env files
+dotenv.config({ path: './config/config.env' })
+
+// Init Express
+const app = express()
+
+const PORT = process.env.PORT || 5000
+// Run Server
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
