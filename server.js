3ab28dd3630e4ffb4a14f53cc724c0329e0326ba
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-20 16:29:33 +0300

    add production middlewares

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const dotenv = require('dotenv')
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
+const morgan = require('morgan')
+const colors = require('colors')
 
 // Routes
 const bootcampRoutes = require('./routes/bootcamp')
@@ -10,9 +19,49 @@ dotenv.config({ path: './config/config.env' })
 // Init Express
 const app = express()
 
+// Body parser
+app.use(express.json())
+
+// Cookie parser
+app.use(cookieParser())
+
+// Sanitize data
+app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+})
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
+
+// Enable CORS
+app.use(cors())
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 // User Routes
 app.use('/api/v1/bootcamps', bootcampRoutes)
 
 const PORT = process.env.PORT || 5000
 // Run Server
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+})
