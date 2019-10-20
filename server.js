694b939c914f75bc75a7bd83e62c8fc181f5208d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-20 19:09:14 +0300

    add Bootcamp model

--- server.js
+++ server.js
@@ -62,11 +62,13 @@ app.use('/api/v1/bootcamps', bootcampRoutes)
 
 const PORT = process.env.PORT || 5000
 // Run Server
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+const server = app.listen(PORT, () =>
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red)
   // Close server & exit process
-  // server.close(() => process.exit(1));
+  server.close(() => process.exit(1))
 })
