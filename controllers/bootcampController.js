e0611609521b0655f4b1a403dd649a29f55595f1
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-21 10:32:28 +0300

    add BootCamp controller ErrorResponse handlers

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -2,14 +2,14 @@ const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorsResponse')
 
 // @GET /api/v1/bootcamps public
-exports.getAllBootCamps = async (req, res) => {
+exports.getAllBootCamps = async (req, res, next) => {
   try {
     const bootCamps = await Bootcamp.find()
 
     res.status(200).json({ count: bootCamps.length, data: bootCamps })
   } catch (error) {
     console.error(error)
-    res.status(400).json({ message: 'Bad request' })
+    next(error)
   }
 }
 
@@ -18,7 +18,7 @@ exports.getBootCampById = async (req, res, next) => {
   try {
     const bootCamp = await Bootcamp.findById(req.params.id)
 
-    if (!bootCamp) return res.status(400).json({ message: 'Bad request' })
+    if (!bootCamp) return next(new ErrorResponse(`BootCamp with id ${req.params.id} not found `, 400))
 
     res.status(200).json(bootCamp)
   } catch (error) {
@@ -28,41 +28,41 @@ exports.getBootCampById = async (req, res, next) => {
 }
 
 // @POST /api/v1/bootcamps Private
-exports.createBootCamp = async (req, res) => {
+exports.createBootCamp = async (req, res, next) => {
   try {
     const bootCamp = await Bootcamp.create(req.body)
 
     res.status(201).json(bootCamp)
   } catch (error) {
     console.error(error)
-    res.status(400).json({ message: 'Bad request' })
+    next(error)
   }
 }
 
 // @PUT /api/v1/bootcamps/:id Private
-exports.updateBootCamp = async (req, res) => {
+exports.updateBootCamp = async (req, res, next) => {
   try {
     const bootCamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-    if (!bootCamp) return res.status(400).json({ message: 'Bad request' })
+    if (!bootCamp) return next(new ErrorResponse(`BootCamp with id ${req.params.id} not found `, 400))
 
     res.status(200).json(bootCamp)
   } catch (error) {
     console.error(error)
-    res.status(400).json({ message: 'Bad request' })
+    next(error)
   }
 }
 
 // @DELETE /api/v1/bootcamps/:id Private
-exports.deleteBootCamp = async (req, res) => {
+exports.deleteBootCamp = async (req, res, next) => {
   try {
     const bootCamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
-    if (!bootCamp) return res.status(400).json({ message: 'Bad request' })
+    if (!bootCamp) return next(new ErrorResponse(`BootCamp with id ${req.params.id} not found `, 400))
 
     res.status(200).json(bootCamp)
   } catch (error) {
     console.error(error)
-    res.status(400).json({ message: 'Bad request' })
+    next(error)
   }
 }
