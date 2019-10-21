5225215b3c7ad6419acd5c54c8ac0a23d97de7e0
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-21 18:51:13 +0300

    add Cascade delete workshops when a bootcamp is deleted

--- models/BootCamp.js
+++ models/BootCamp.js
@@ -118,12 +118,12 @@ BootcampSchema.pre('save', async function(next) {
   next()
 })
 
-// Cascade delete courses when a bootcamp is deleted
-// BootcampSchema.pre('remove', async function(next) {
-//   console.log(`Courses being removed from bootcamp ${this._id}`)
-//   await this.model('Course').deleteMany({ bootcamp: this._id })
-//   next()
-// })
+// Cascade delete workshops when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+  console.log(`Workshops removed from bootcamp ${this._id}`)
+  await this.model('Workshop').deleteMany({ bootcamp: this._id })
+  next()
+})
 
 // Reverse populate with virtuals
 BootcampSchema.virtual('workshops', {
