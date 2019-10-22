f3eedb8271794ae4be47ef6a525878d9411d32c6
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-22 16:28:50 +0300

    add loginUser handler and route

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,8 @@ const asyncMiddleware = require('../middlewares/asyncMiddleware')
 exports.registerUser = asyncMiddleware(async (req, res, next) => {
   const { email, password, role, name } = req.body
 
-  const checkUser = await User.find({ email })
+  const checkUser = await User.findOne({ email })
+
   if (checkUser) return next(new ErrorResponse('Invalid credentials', 401))
 
   // Create user
@@ -18,5 +19,31 @@ exports.registerUser = asyncMiddleware(async (req, res, next) => {
     role
   })
 
-  res.status(200).json({ token: '', data: user })
+  // Create token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ token, data: user })
+})
+
+// @POST Login user
+// Route: /api/v1/auth/register
+exports.loginUser = asyncMiddleware(async (req, res, next) => {
+  const { email, password } = req.body
+
+  // Validate email and password
+  if (!email || !password) return next(new ErrorResponse('Invalid email or password', 400))
+
+  // Check for usex exists
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user) return next(new ErrorResponse('Invalid credentials', 401))
+
+  // Compare passwords
+  const isMatch = await user.matchPassword(password)
+  if (!isMatch) return next(new ErrorResponse('Invalid email or password', 400))
+
+  // Create token
+  const token = user.getSignedJwtToken()
+
+  res.status(200).json({ token, data: user })
 })
