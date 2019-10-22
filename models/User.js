8a6d776357d16f48315488876b5ef5b445eb46ef
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-22 16:28:50 +0300

    add loginUser handler and route

--- models/User.js
+++ models/User.js
@@ -44,7 +44,7 @@ UserSchema.pre('save', async function(next) {
   this.password = await bcrypt.hash(this.password, salt)
 })
 
-// Sign JWT and return
+// Sign JWT and return token
 UserSchema.methods.getSignedJwtToken = function() {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE
