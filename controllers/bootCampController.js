f8719fd9dcdb44ac27dacf36c5055e7e6f2c8ada
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-22 14:49:18 +0300

    add reqResMiddleware

--- controllers/bootCampController.js
+++ controllers/bootCampController.js
@@ -6,69 +6,7 @@ const path = require('path')
 
 // @GET /api/v1/bootcamps public
 exports.getAllBootCamps = asyncMiddleware(async (req, res) => {
-  let query
 
-  // Make copy of query
-  const reqQueryCopy = { ...req.query }
-
-  // Fields to remove from query
-  const removeFields = ['select', 'sort', 'page', 'limit']
-
-  // Loop over removeFields and delete them from reqQueryCopy
-  removeFields.forEach((param) => delete reqQueryCopy[param])
-
-  // Create query string
-  let queryString = JSON.stringify(reqQueryCopy)
-
-  // Create operators ($gt, $gte, etc)
-  queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
-
-  query = Bootcamp.find(JSON.parse(queryString)).populate('workshops')
-
-  // Add select BootCamp.find().select()
-  if (req.query.select) {
-    const selectParams = req.query.select.split(',').join(' ')
-    query = query.select(selectParams)
-  }
-
-  // Add sort BootCamp.find().sort()
-  if (req.query.sort) {
-    const sortParams = req.query.sort.split(',').join(' ')
-    query = query.sort(sortParams)
-  } else {
-    query = query.sort('-createdAt')
-  }
-
-  // Pagination
-  const page = parseInt(req.query.page, 10) || 1
-  const limit = parseInt(req.query.limit, 10) || 10
-  const startIndex = (page - 1) * limit
-  const endIndex = page * limit
-  const total = await Bootcamp.countDocuments()
-
-  query = query.skip(startIndex).limit(limit)
-
-  // make db request
-  const bootCamps = await query
-
-  // Pagination result for response data only
-  const pagination = {}
-
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit
-    }
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit
-    }
-  }
-
-  res.status(200).json({ count: bootCamps.length, pagination, data: bootCamps })
 })
 
 // @GET /api/v1/bootcamps/:id public
