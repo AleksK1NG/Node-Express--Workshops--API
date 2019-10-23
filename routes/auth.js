d353728bf7115a7207d508fd9e779b708714c323
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-23 10:44:26 +0300

    add getCurrentUser handler and route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
+const auth = require('../middlewares/authMiddleware')
 
 router.post('/register', authController.registerUser)
 router.post('/login', authController.loginUser)
+router.get('/me', auth.authMiddleware, authController.getCurrentUser)
 
 module.exports = router
