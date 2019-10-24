1cc1fc1f7966e6aa2d5192f59cc5ef847e221d18
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-24 11:08:55 +0300

    add Review updateReview handler and route

--- models/Review.js
+++ models/Review.js
@@ -33,7 +33,7 @@ const ReviewSchema = new mongoose.Schema({
   }
 })
 
-// Prevent user from submitting more than one review per bootcamp
+// Set user can add only one review for bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true })
 
 // Static method to get avg rating and save
